a7d5fb2a128977d1e191e8ca124cdddb4391bb77
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2024-03-16 00:17:36 +0500

    remove nvim config linking

--- setup.sh
+++ setup.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
 # please use the ansible playbook at https://github.com/chaudhryjunaid/setup-macos to configure
-# this below, is a hack!
-mkdir -p ~/.config/nvim
+# this script is only for special use
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # link tmux config
 ln -sf $SCRIPT_DIR/tmux.conf ~/.tmux.conf
-
-# link nvim config
-ln -sf $SCRIPT_DIR/nvim/init.vim ~/.config/nvim/init.vim
 
 # link vim config
 ln -sf $SCRIPT_DIR/vimrc ~/.vimrc
